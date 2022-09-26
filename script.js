a6df6befe6aff74d5dbcc6acd755e85bf28cb848
Miguel Angel Vivanco <vivancomiguel119@gmail.com>
2022-09-26 10:52:30 -0500

    feat: uso de script extra

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("saoko papi saoko");
+document.getElementById("generation").style.color = "gray";
+function colorTitulo(){
+  document.getElementById("generation").style.color = "blue";
+}
